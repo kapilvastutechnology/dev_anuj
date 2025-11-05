9bd1ba337df5a8fdf23715a290a5f6b3b18fb771
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-11-05 10:12:16 +0545

    update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,17 +1,23 @@
 import User from "../models/user.js";
+import bcrypt from 'bcrypt';
 
 export const loginUser = (req, res) =>{
-
+    
 }
 
 export const registerUser = async (req,res) =>{
     const {email, password, username} = req.body ?? {};
     try {
+        const hashPass = bcrypt.hashSync(password,10);
         await User.create({
             email,
             password,
             username
         });
+        return res.status(201).json({
+            status: 'success',
+            data:'user successfully register'
+        })
     } catch (err) {
         return res.status(400).json({
             status: 'error',
