8a238cd0d4842b70d1333b1e8cd69f084e987b15
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-11-06 00:10:18 +0545

    update

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,27 +1,32 @@
-import User from "../models/user.js";
+import User from "../models/User.js";
 import bcrypt from 'bcrypt';
 
-export const loginUser = (req, res) =>{
-    
+
+
+
+export const loginUser = (req, res) => {
+
 }
 
-export const registerUser = async (req,res) =>{
-    const {email, password, username} = req.body ?? {};
-    try {
-        const hashPass = bcrypt.hashSync(password,10);
-        await User.create({
-            email,
-            password,
-            username
-        });
-        return res.status(201).json({
-            status: 'success',
-            data:'user successfully register'
-        })
-    } catch (err) {
-        return res.status(400).json({
-            status: 'error',
-            data:err.message
-        })
-    }
+export const registerUser = async (req, res) => {
+  const { email, password, username } = req.body ?? {};
+
+  try {
+    const hashPass = bcrypt.hashSync(password, 10);
+    await User.create({
+      email,
+      password: hashPass,
+      username
+    });
+    return res.status(201).json({
+      status: 'success',
+      data: 'user successfully registered'
+    })
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      data: err.message
+    });
+  }
+
 }
