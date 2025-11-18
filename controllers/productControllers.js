2360f9b3bbd00443a8e4665f90d365b22308d5f2
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-11-18 21:11:13 +0545

    change

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -140,7 +140,7 @@ export const deleteProduct = async (req, res) => {
   } catch (err) {
     return res.status(500).json({
       status: 'error',
-      data: err.message
+      message: err.message
     });
 
   }
