efb0f1934c08e1fde9678c7ae2e2e43d51c3b0bc
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-11-20 10:48:35 +0545

    change

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use(fileUpload({
   limits: { fileSize: 5 * 1024 * 1024 },
 }));
 
-// app.use(express.static('uploads'));
-app.use("/uploads", express.static("uploads"));
+app.use(express.static('uploads'));
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
