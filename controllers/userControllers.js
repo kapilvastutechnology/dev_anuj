d8ce4964214fa446a1d913dfe789db569f819edf
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-12-02 22:54:49 +0545

    change

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,6 +2,48 @@ import User from "../models/User.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+export const getUser = async (req, res) =>{
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    return res.status(200).json({
+      status:  'success',
+      user
+    })
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      message: err.message
+    })
+  }
+}
+
+export const updateProfile = async (req, res) =>{
+  const {email, username} = req.body ?? {};
+
+  try {
+    const isExist = await User.findById(req.userId);
+    if(!isExist) return res.status(404).json({
+      status: 'error',
+      message: 'user doesn\'t exist'
+    });
+
+    isExist.email = email || isExist.email;
+    isExist.username = username || isExist.username;
+    await isExist.save(); 
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'profile updated successfully'
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      message: err.message
+    });
+  }
+}
+
 export const loginUser = async (req, res) => {
   const { email, password } = req.body ?? {};
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { notAllowed } from '../utils/notAllowed.js';
-import { loginUser, registerUser } from '../controllers/userControllers.js';
+import { getUser, loginUser, registerUser, updateProfile } from '../controllers/userControllers.js';
+import { checkUser } from '../middlewares/checkUser.js';
 const router = express.Router();
 router.route('/api/users/login').post(loginUser).all(notAllowed);
 router.route('/api/users/register').post(registerUser).all(notAllowed);
-
+router.route('/api/users').get(checkUser,getUser).patch(checkUser,updateProfile).all(notAllowed);
 export default router;
