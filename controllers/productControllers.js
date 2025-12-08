166d39112ab88913736cc7189320fa1e3ff7e89d
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-12-09 00:19:22 +0545

    change

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,4 +1,4 @@
-import Product  from "../models/Product.js"
+import Product, { brands, categories }  from "../models/Product.js"
 import fs from 'fs';
 
 // export const getProducts = async (req,res)=>{
@@ -73,24 +73,165 @@ import fs from 'fs';
 //     }
 // }
 
-export const getProducts = async(req,res) => {
+// export const getProducts = async(req,res) => {
+
+//   try {
+//     const excludedFileds = ['page', 'limit', 'sort', 'fields', 'skip', 'search'];
+//     let queryObj = {...req.query};
+
+//     excludedFileds.forEach((val) => {
+//       delete queryObj[val];
+//     })
+
+
+//     // search method
+//     if (req.query.search) {
+//        const searchText = req.query.search;
+
+//       if (categories.some((name) => name.toLowerCase() === searchText.toLowerCase())) {
+//          queryObj.category = { $regex: searchText, $options: 'i' };
+
+//          } else if (brands.some((name) => name.toLowerCase() === searchText.toLowerCase())) {
+//          queryObj.brand = { $regex: searchText, $options: 'i' };
+//        } else {
+//          queryObj.title = { $regex: searchText, $options: 'i' };
+//        }
+
+//      }
+
+
+//     // {'rating[gt]': '4'} and {rating: {$gt:4}} for conditions 
+//   const output = Object.entries(queryObj).reduce((acc, [key, value]) => {
+//       const match = key.match(/^(.+)\[(.+)\]$/);  // <-- FIXED REGEX
+//       if (match) {
+//         const field = match[1];
+//         const operator = `$${match[2]}`;
+//         const parsedValue = isNaN(value) ? value : Number(value);
+
+//         acc[field] = { [operator]: parsedValue };
+//       } else {
+//         acc[key] = value;
+//       }
+//       return acc;
+//     }, {});
+
+
+
+// // sort method
+//     let query = Product.find(output);
+//     if(req.query.sort){
+//       const sortBy = req.query.sort.split('.').join('');
+//       query = query.sort(sortBy)
+//     }
+
+// // select method
+//     if(req.query.fields){
+//       const fields = req.query.fields.split(',').join('');
+//         query = query.select(fields);
+//     }
+
+//     // page filter
+//     const page = req.query.page || 1;
+//     const limit = req.query.limit || 10;
+//     const skip = (page-1) * 10;
+
+//     const total = Product.countDocuments();
+//     const products =  await query.skip(skip).limit(limit);
+    
+//     return res.status(200).json({
+//       status: 'success',
+//       total,
+//       products,
+//       totalPages: Math.ceil(total / limit)
+//     });
+
+//   } catch (err) {
+//       return res.status(400).json({
+//         status: 'error',
+//         data: err.message
+//       });
+//   }
+// }
+
+export const getProducts = async (req, res) => {
   try {
-    console.log(req.query);
-    const products = await Product.find({});
+
+    const exludedFields = ['page', 'limit', 'sort', 'fields', 'skip', 'search'];
+    let queryObj = { ...req.query };
+
+    exludedFields.forEach((val) => {
+      delete queryObj[val];
+    })
+
+
+
+    if (req.query.search) {
+      const searchText = req.query.search;
+
+
+      if (categories.some((name) => name.toLowerCase() === searchText.toLowerCase())) {
+        queryObj.category = { $regex: searchText, $options: 'i' };
+
+      } else if (brands.some((name) => name.toLowerCase() === searchText.toLowerCase())) {
+        queryObj.brand = { $regex: searchText, $options: 'i' };
+      } else {
+        queryObj.title = { $regex: searchText, $options: 'i' };
+      }
+
+
+    }
+
+    // { 'rating[gt]': '4' }
+    // {rating: {$gt: 4}}
+    const output = Object.entries(queryObj).reduce((acc, [key, value]) => {
+      const match = key.match(/^(.+)\[(.+)\]$/);  // <-- FIXED REGEX
+      if (match) {
+        const field = match[1];
+        const operator = `$${match[2]}`;
+        const parsedValue = isNaN(value) ? value : Number(value);
+
+        acc[field] = { [operator]: parsedValue };
+      } else {
+        acc[key] = value;
+      }
+      return acc;
+    }, {});
+
+
+    let query = Product.find(output);
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    }
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * 10;
+
+    const total = await Product.countDocuments();
+    const products = await query.skip(skip).limit(limit);
+
     return res.status(200).json({
       status: 'success',
-      products
+      total,
+      products,
+      totalPages: Math.ceil(total / limit)
     });
+
+
   } catch (err) {
-      return res.status(400).json({
-        status: 'error',
-        data: err.message
-      });
+    return res.status(400).json({
+      status: 'Error',
+      data: err.message
+    });
   }
 }
-
-
-
 
 
 
